a583b2ab8b94ee883cd87eece081eda05a4edbaf
INKWWW <whmink@foxmail.com>
2018-06-21 15:00:54 +0800

    update

--- run_model_server.py
+++ run_model_server.py
@@ -116,86 +116,13 @@ def operation(model, test_sentence_1, test_sentence_2, distance_model):
     
 
 if __name__ == '__main__':
-    dirname = os.getcwd()
-    # model_name = '\\word2vec_model.model'
-    # model_name = '\\word2vec_model_10000.model'
-    model_name = '\\word2vec_model_10000_skip_stopwords.model'
-    # model_name = '\\word2vec_model_10000_skip.model'
-    model_output = dirname + model_name
 
-    # test_sentence_1 = '哈尔滨久久华康科技有限公司'
-    # test_sentence_2 = '哈尔滨久久华康有限公司'  # cos：0.9999999999999998  .0  enclidean：0.10381817865051336  V2:0.1393186701868827
-    # test_sentence_2 = '哈尔滨长久华康科技有限公司'  # cos：1.0  0.0  enclidean：0.0  V2:0.0
-    # test_sentence_2 = '北京市百融金服科技有限公司'  # cos：0.9999839620230886  0.043129391793413574  enclidean：0.027304886398964206  V2:0.030312174677792212
-    # test_sentence_2 = '北京市百融金服科技公司'  # cos：0.9999839620230886  0.043129391793413574  enclidean：0.027304886398964206  V2:0.11785001169482552
-    # test_sentence_2 = '四川省百融金服科技有限公司'  # cos：  0.07568612905089231  enclidean：0.0520855644828885  V2:0.12377131385837323  0.02897386663929932
-    # test_sentence_2 = '四川省百融金服有限公司'  # cos：  0.07568612905089231  enclidean：0.16771822181146087  V2:0.2934710911310562  0.1832938640796101
+    model_output = '../word2vec_model_skip_stopwords.model'
 
-    # # 第一组测试
-    # test_sentence_1 = '哈尔滨久久华康科技有限公司'
-    # test_sentence_2 = '哈尔滨久久华康有限公司'  # 0.0
-    # test_sentence_2 = '哈尔滨长久华康科技有限公司'  # 0.022203229313000852   0.0
-    # test_sentence_2 = '哈尔滨长久康华科技有限公司'  # 0.05328034558352251  0.007523175114716447  0.008065828246018075
-
-    # test_sentence_2 = '黑龙江长久康华科技有限公司'  # 0.042524020642892055  0.07734101603986997
-    # test_sentence_2 = '黑龙江久久康华科技有限公司'  # 0.030177443023447763  0.07734101603986997
-    # test_sentence_2 = '黑龙江久久华康科技有限公司'  # 0.11105165879285421  0.10335089333917838
-
-    # test_sentence_2 = '北京市百融金服科技有限公司'  # 0.02750834860840494  0.017958557949121177  0.013522223661126041
-    # test_sentence_2 = '北京市百融金服科技公司'  # 0.02750834860840494  0.017958557949121177  
-    # test_sentence_2 = '四川省百融金服科技有限公司'  # 0.09338489938119292  0.10301095735634219  0.014167722860191728
-    # # test_sentence_2 = '四川省百融金服有限公司'  # 0.09338489938119292  0.10301095735634219  0.014167722860191728
-
-    # 第二组测试
-    test_sentence_1 = '中兴通讯'
-    test_sentence_2 = '西安中兴新软件有限责任公司'
-    # test_sentence_2 = '天津之海能源发展股份有限公司'
-    # test_sentence_2 = '北京市红兴隆农垦民乐农业生产资料有限公司'  # 0.0
-    # test_sentence_2 = '黑龙江红兴隆农垦民乐农业有限公司'  # 0.037899128553774  0.0
-    # test_sentence_2 = '黑龙江红兴隆农垦民乐有限公司'  # 0.10465808393829548  0.15634690247603167  0.022769100045612813
-    # test_sentence_2 = '黑龙江红兴隆农垦民乐生产资料有限公司'  # 0.12483684740017804  0.15634690247603167  0.022769100045612813
-    # test_sentence_2 = '黑龙江红隆农垦民乐农业有限公司'  # 0.09307739644613543  0.08167516464292726  0.005218276287039758
-    # test_sentence_2 = '黑龙江红兴隆农垦民乐农业有限公司'  # 0.0
-    # test_sentence_2 = '黑龙江兴隆农垦民乐农业生产资料有限公司'
-
-    # 第三组测试
-    # test_sentence_1 = '蚌埠悠然酒店管理有限公司'
-    # test_sentence_2 = '四川省蚌埠悠然酒店管理有限公司'
-    # test_sentence_2 = '安徽省蚌埠华康悠然酒店管理公司'
-    # test_sentence_2 = '安徽省蚌埠悠然酒店管理有限公司'  # 0.9777817058322253
-    # test_sentence_2 = '安徽省蚌埠四凤悠然酒店管理有限公司'  # 0.011131538396269902
-    # test_sentence_1 = '四川省'
-    # test_sentence_2 = '安徽省美丽' # 0.3025576495178356
-    # test_sentence_2 = '安徽省'  # 0.3025576495178356
-    # test_sentence_1 = '有限公司'
-    # test_sentence_2 = '有限公司'  # 
-    # test_sentence_1 = '中医院'
-    # test_sentence_2 = '中医医院'
-    # test_sentence_1 = '黑龙江'
-    # test_sentence_2 = '黑龙江省'
-    # test_sentence_1 = '蚌埠'
-    # test_sentence_2 = '安徽省'
-
+    test_sentence_1 = '北京'
+    test_sentence_2 = '北京市'
+    
     # 加载模型
     w2v_model = loadModel(model_output)
-    operation(w2v_model, test_sentence_1, test_sentence_2)
-
-    # 分词
-    # test_sentence = '哈尔滨久久华康科技有限公司'
-    # parser_list = parserSen(test_sentence)
-
-    # # 测试模型
-    # # test_word = ['哈尔滨久久华康科技有限公司']
-    # test_word = '哈尔滨'
-    # findWordVec(w2v_model, test_word)
-    # test_word = '公司'  # -7.354347
-    # test_word = '科技'  # -4.7045946
-    # test_word = '哈尔滨'  # -1.788831
-    # test_word = '康华'  # -0.049786568
-    # test_word = '有限公司'  # -11.7632475
-    # test_word = '北京'  # -6.831831
-    # test_word = '四川'  # -3.8586986
-    # test_word = '四川省'  # -0.9724991
-    # test_word = '北京市'  # -1.9034648
-    # findWordVec(w2v_model, test_word)
- 
+    distance_model = 2
+    operation(w2v_model, test_sentence_1, test_sentence_2, distance_model)
